236cb521bef74191bd4f135b18e0c97a62873ea7
Blake Burch <blake@shipyardapp.com>
2022-11-08 14:23:26 -0600

    Minor printed message update.

--- shipyardapi_blueprints/download_voyages.py
+++ shipyardapi_blueprints/download_voyages.py
@@ -4,19 +4,19 @@ import argparse
 import sys
 import os
 try:
-    import exit_codes as ec 
+    import exit_codes as ec
 except BaseException:
     from . import exit_codes as ec
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--organization-id', dest = 'org_id', required = True)
-    parser.add_argument("--api-key", dest = 'api_key', required = True)
-    parser.add_argument("--file-name", dest = "file_name", required = True)
-    parser.add_argument('--folder-name', dest = 'folder_name', required = False)
+    parser.add_argument('--organization-id', dest='org_id', required=True)
+    parser.add_argument("--api-key", dest='api_key', required=True)
+    parser.add_argument("--file-name", dest="file_name", required=True)
+    parser.add_argument('--folder-name', dest='folder_name', required=False)
     return parser.parse_args()
- 
+
 
 def form_url(organization_id):
     """
@@ -25,13 +25,13 @@ def form_url(organization_id):
     return f"https://api.app.shipyardapp.com/orgs/{organization_id}/voyages"
 
 
-def form_request(url,api_key):
+def form_request(url, api_key):
     """
     Forms the GET request to the shipyard API and returns the response in CSV format
     """
-    headers = {"X-Shipyard-API-Key":api_key}
+    headers = {"X-Shipyard-API-Key": api_key}
     try:
-        results = requests.get(url, headers = headers)
+        results = requests.get(url, headers=headers)
         return results
 
     except Exception as e:
@@ -40,28 +40,28 @@ def form_request(url,api_key):
         sys.exit(ec.EXIT_CODE_INVALID_CREDENTIALS)
 
 
-def write_file(response_txt, file, folder_name = None):
+def write_file(response_txt, file, folder_name=None):
     """
-    Writes the csv response from the api to a file. If the folder name is not provided, then it will be 
+    Writes the csv response from the api to a file. If the folder name is not provided, then it will be
     written in the current working directory
     """
-    ## ensure that the file type is a csv
+    # ensure that the file type is a csv
     if str(file).find('.csv') == -1:
         print("Error: The file name must be a csv. Please add the .csv suffix to the file")
         sys.exit(ec.EXIT_CODE_INVALID_FILE_TYPE)
     if folder_name is not None:
-        ## create the directory if it does not exist
+        # create the directory if it does not exist
         shipyard.create_folder_if_dne(folder_name)
-        dest_path = shipyard.combine_folder_and_file_name(folder_name,file)
+        dest_path = shipyard.combine_folder_and_file_name(folder_name, file)
         message_path = os.path.join(os.getcwd(), folder_name, file)
         message = f"Saved {file} in {os.path.normpath(message_path)}"
     else:
         dest_path = file
-        message_path = os.path.join(os.getcwd(),file)
+        message_path = os.path.join(os.getcwd(), file)
         message = f"Saved {file} in {os.path.normpath(message_path)}"
-    with open(dest_path,'w') as csv_file:
+    with open(dest_path, 'w') as csv_file:
         csv_file.write(response_txt)
-    print(f"Saved {file} to {message}")
+    print(f"Saved voyages to {message_path}")
 
 
 def main():
@@ -71,11 +71,9 @@ def main():
     file_name = args.file_name
     folder_name = args.folder_name
     url = form_url(org_id)
-    response = form_request(url,api_key)
+    response = form_request(url, api_key)
     write_file(response.text, file_name, folder_name)
 
 
 if __name__ == '__main__':
     main()
-
-
